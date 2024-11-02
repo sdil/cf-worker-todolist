6d9d297e5551939990f8541b17e7ec31e04dab52
Mohamad Fadhil <piukul@yahoo.com.my>
2024-11-02 12:02:27 +0800

    connect api to todo service

--- api-todo/src/index.ts
+++ api-todo/src/index.ts
@@ -2,8 +2,9 @@ import { Hono } from 'hono'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/todos', async (c) => {
+  const todos = await c.env.TODO_SERVICE.getTodos('1')
+  return c.json(todos)
 })
 
 export default app
